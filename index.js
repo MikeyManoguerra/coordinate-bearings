2ea5e1939241070502730865487b2d7f81fba70e
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-04-29 23:47:27 -0400

    can add a dataset to db

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const morgan = require('morgan');
+const PORT = 3000;
+const dataSetsRouter = require('./routes/datasets');
+const routesRouter = require('./routes/routes');
+const { dbConnect } = require('./mongoose.js');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+
+
+app.use(
+  morgan('common')
+);
+
+
+app.use('/api/datasets', dataSetsRouter);
+// app.use('/api/routes', routesRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
@@ -20,7 +33,21 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+function runServer(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
 
 
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+module.exports = { app };
---
+++ routes/datasets.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const DataSet = require('../models/dataSet.js');
+const Point = require('../models/point.js');
+// get the names of all the datasets
+
+router.get('/', (req, res, next) => {
+  return DataSet.find()
+    .then((dataSets) => {
+      console.log(dataSets);
+      return res.json(dataSets);
+    })
+    .catch(err => next(err));
+});
+
+router.post('/', (req, res, next) => {
+  const {
+    name,
+    description,
+    coordinatesArray
+  } = req.body;
+  const newDataSet = { name, description };
+
+  return DataSet.create(newDataSet)
+    .then((result) => {
+      const dataSetId = result.id;
+      return insertPointIntoDataBase(dataSetId, coordinatesArray);
+    })
+    .then(() => {
+      return res.status(204).end();
+    })
+    .catch((err) => {
+
+      console.log('hi', err);
+      return next(err);
+    });
+});
+
+
+// TODO : this is bad fix the catch block
+function insertPointIntoDataBase(dataSetId, coordinatesArray) {
+  return coordinatesArray.forEach(plotPoint => {
+    const newPoint = {
+      xCoordinate: plotPoint.xCoordinate,
+      yCoordinate: plotPoint.yCoordinate,
+      dataSetId
+    };
+    return Point.create(newPoint)
+      .catch((err) => { throw err })
+  })
+    .catch((res) => {
+      console.log('hey', res)
+      const error = new Error(' one of the plot points you entered is missing a value')
+      error.status = 400;
+      return error;
+    });
+
+}
+
+
+module.exports = router;
